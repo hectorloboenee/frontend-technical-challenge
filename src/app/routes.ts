ccf360472cf305179bb50106ea7f86367f64f652
Hector Lobo <hector.lobo.enee@gmail.com>
2026-02-01 00:22:34 -0600

    feat: retrive zipe code, set custom components

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,16 @@
 import type {RouteRecordRaw} from "vue-router";
 import ZipCodeSearcher from "./modules/zip-code-searcher/zip-code-searcher.vue";
+import Home from "./modules/home/home.vue";
 
 export const routes: RouteRecordRaw[] = [
     {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+    {
         path: '/zip-code-searcher',
+        name: 'zip-code-searcher',
         component: ZipCodeSearcher,
     }
 ]
