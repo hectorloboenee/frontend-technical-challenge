9abfb60eeae9e94c815f118a4390c2f26de3a6de
Hector Lobo <hector.lobo.enee@gmail.com>
2026-02-02 01:49:07 -0600

    feat: zip code create fixed

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,7 @@
 import type { RouteRecordRaw } from 'vue-router';
-import ZipCodeSearcher from './modules/zip-code-searcher/zip-code-searcher.vue';
+import ZipCodeSearcher from './modules/zip-codes/zip-code-searcher/zip-code-searcher.vue';
 import Home from './modules/home/home.vue';
-import ZipCodes from './modules/zip-codes/zip-codes.vue';
+import ZipCodes from './modules/zip-codes/list-zip-codes/zip-codes.vue';
 
 export const routes: RouteRecordRaw[] = [
   {
