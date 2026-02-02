b4b08940e9fee703518606bf138b4e951220ecdc
Hector Lobo <hector.lobo.enee@gmail.com>
2026-02-01 23:25:20 -0600

    feat: create user added

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {router} from "./router.ts";
-import {store} from './store.ts';
+import store from './store.ts';
 
 createApp(App)
     .use(router)
